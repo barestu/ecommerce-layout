c21e3a98b4f543129857644a16e4fb3784ec9e96
barestu <fheartnet@gmail.com>
2018-04-15 21:08:19 +0700

    Create server api

---
+++ server/controllers/users_controller.js
@@ -0,0 +1,59 @@
+const User = require('../models/User')
+const key = process.env.KEY
+
+module.exports = {
+  register: function(req, res) {
+    let newUser = new User ({
+      username: req.body.username,
+      password: req.body.password,
+      role: req.body.role
+    })
+
+    newUser.save()
+    .then(success => {
+      res.status(201).send({
+        message: 'Register user success',
+        data: newUser
+      })
+    })
+    .catch(err => {
+      res.status(400).send({
+        message: 'Register user failed',
+        detail: err.message
+      })
+    })
+  },
+
+  login: function(req, res) {
+    User.findOne({
+      username: req.body.username
+    })
+    .then(user => {
+      let checkLogin = bcrypt.compareSync(req.body.password, user.password)
+
+      if (checkLogin == true) {
+        let token = jwt.sign({
+          username: user.username
+        }, key)
+  
+        res.status(200).send({
+          message: 'Login success',
+          token: token
+        })
+      } else {
+        res.status(400).send({
+          message: 'Login failed',
+          detail: err.message
+        })
+      }
+    })
+    .catch(err => {
+      console.log('not found', err)
+
+      res.status(400).send({
+        message: 'Login failed',
+        detail: err.message
+      })
+    })
+  }
+}
